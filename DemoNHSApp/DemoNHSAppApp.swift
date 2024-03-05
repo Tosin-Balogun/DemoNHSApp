50f257ceb1b52e2ee06eb786474523cc05f793ff
Tosin <71393402+Tosin-Balogun@users.noreply.github.com>
2024-03-05 07:19:38 +0000

    Initial Commit

---
+++ DemoNHSApp/DemoNHSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoNHSAppApp.swift
+//  DemoNHSApp
+//
+//  Created by Tosin Balogun on 05/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct DemoNHSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
