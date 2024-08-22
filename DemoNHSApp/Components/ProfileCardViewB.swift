04b5a7f63221f345a5a540f5dcd2ea3256bf36ee
Tosin <71393402+Tosin-Balogun@users.noreply.github.com>
2024-08-22 14:15:23 +0100

    Add blue tabBar

---
+++ DemoNHSApp/Components/ProfileCardViewB.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileCardViewB.swift
+//  DemoNHSApp
+//
+//  Created by Tosin Balogun on 17/04/2024.
+//
+
+import SwiftUI
+
+struct ProfileCardViewB: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileCardViewB()
+}
---
+++ DemoNHSApp/Your Health/Upcoming & past appointments/UpcomingAndPastAppointmentsView.swift
@@ -0,0 +1,18 @@
+//
+//  UpcomingAndPastAppointmentsView.swift
+//  DemoNHSApp
+//
+//  Created by Tosin Balogun on 13/03/2024.
+//
+
+import SwiftUI
+
+struct UpcomingAndPastAppointmentsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UpcomingAndPastAppointmentsView()
+}
