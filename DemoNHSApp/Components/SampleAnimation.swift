cc1793349c053b700c524c6eebfafed2f41eec6a
Tosin <71393402+Tosin-Balogun@users.noreply.github.com>
2025-03-29 15:18:12 +0000

    Update animation

--- DemoNHSApp/Components/SampleAnimation.swift
+++ DemoNHSApp/Components/SampleAnimation.swift
@@ -16,9 +16,9 @@ struct SampleAnimation: View {
             //Something here
 //            LottieView(animation: .named("RabbitAnimation"))
 //                       .playing()
-            DotLottieAnimation(fileName: "NewAccount", config: AnimationConfig(autoplay: true, loop: true)).view()
+            DotLottieAnimation(fileName: "AccountUpdateAnimation", config: AnimationConfig(autoplay: true, loop: true)).view()
         }
-        .frame(height: 450.0)
+        .frame(height: 350.0)
         
 
 
